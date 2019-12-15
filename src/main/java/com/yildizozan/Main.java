f93a5c49441a103638aa9d2670702d11c652e1cb
Ozan Yıldız <developer@yildizozan.com>
2019-12-15 23:08:39 +0200

    Main class was added.

---
+++ src/main/java/com/yildizozan/Main.java
@@ -0,0 +1,13 @@
+package com.yildizozan;
+
+public class Main {
+	public static void main(String[] args) {
+		int size = 16;
+
+		ComplexNumber temp = new ComplexNumber(0,0);
+
+		Matrix matrix = new Matrix(16);
+
+		System.out.println(matrix);
+	}
+}
